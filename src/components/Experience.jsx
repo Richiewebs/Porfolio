dc327b07f29f14b7555c8cf2b440d48c2733e2ba
Richiewebs <asmahrichelle9@gmail.com>
2024-07-07 22:26:05 +0000

    small changes

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -6,27 +6,30 @@ const Experience = () => {
   return (
     <div className='w-full bg-exbg p-6 shadow-md'>
       <h1 className='text-btn2 font-bold text-2xl text-center pb-5'>Education & Experience</h1>
-      <div className='p-10'>
-        <div className='flex items-start space-y-28 flex-col'>
-          <div className='flex items-center mb-4'>
+      <div className='p-10 flex'>
+        <div className='flex items-start space-y-28'>
+          <div className='flex h-54  space-y-20 flex-col items-center'>
             <div className='w-42 flex items-center mr-4'>
-              <img src={umat} alt="Umat" className='mr-2' />
-              <p className='font-bold'>Umat</p>
+              <img src={umat} alt="Umat" className='mr-5'/>
+              <p className='font-bold text-licol'>Umat</p>
             </div>
-            <ul className="vertical-line-list list-disc space-y-2">
-              <li className='text-xl text-licol'>Computer Science and Engineering</li>
-            </ul>
-          </div>
-          <div className='flex items-center mb-4'>
+            
             <div className='w-42 flex items-center mr-4'>
               <img src={aaenics} alt="Aaenics" className='mr-2' />
-              <p className='font-bold'>Aaenics</p>
+              <p className='font-bold text-licol'>Aaenics</p>
             </div>
-            <ul className="vertical-line-list list-disc space-y-2">
+          </div>
+          
+        </div>
+        
+        <div className='h-54 flex flex-col mt-5'>
+            
+            <ul className="vertical-line-list list-disc space-y-40 ">
+              <li className='text-xl text-licol '>Computer Science and Engineering</li>
+          
               <li className='text-xl text-licol'>Tutor at Aaenics Robotics Club</li>
             </ul>
           </div>
-        </div>
       </div>
     </div>
   );
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='bg-footercol p-20 flex place-content-around'>
-        <div className='flex flex-col text-white'>
+    <div className='bg-footercol p-20 h-72 flex place-content-around'>
+        <div className='flex flex-col text-white space-y-5'>
             <h1 className='font-bold'>Contact Info</h1>
             <p>+233 839 234 2342</p>
             <p>asmahrichelle9@gmail.com</p>
